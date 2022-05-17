d319dfb98070d36edfdeec4b5715367937c1457a
InmookJeong <jeonginmook@gmail.com>
2022-05-17 16:11:21 +0900

    feat: add modify reply in rest controller
    - 댓글 수정을 위한 메소드 추가

--- SpringFramework/021001_SpringFramework_게시판을_통한_연습/workspace/UserBoardProject/src/main/java/kr/study/dev_mook/controller/ReplyController.java
+++ SpringFramework/021001_SpringFramework_게시판을_통한_연습/workspace/UserBoardProject/src/main/java/kr/study/dev_mook/controller/ReplyController.java
@@ -56,4 +56,19 @@ public class ReplyController {
 		return entity;
 	}
 	
+	
+	@RequestMapping(value = "/{rno}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<String> update(@PathVariable("rno") Integer rno, @RequestBody ReplyVO vo) {
+		ResponseEntity<String> entity = null;
+		try {
+			vo.setRno(rno);
+			service.modifyReply(vo);
+			entity = new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			entity = new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+		}
+		return entity;
+	}
+	
 }
