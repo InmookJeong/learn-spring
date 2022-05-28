bb70a9815a98ce744eb3e7700493ff6792d552e3
InmookJeong <jeonginmook@gmail.com>
2022-05-28 16:35:22 +0900

    feat: add post login redirect
    - 로그인 처리 후 redirect 처리
    - 왜 정상적으로 동작하지 않는지 나중에 확인하기

---
+++ SpringFramework/021001_SpringFramework_게시판을_통한_연습/workspace/UserBoardProject/src/main/java/kr/study/dev_mook/interceptor/AuthInterceptor.java
@@ -0,0 +1,51 @@
+package kr.study.dev_mook.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
+	private static final String LOGIN = "login";
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		HttpSession session = request.getSession();
+		
+		// 사용자가 로그인 상태인지 확인
+		if(session.getAttribute("login") == null) {
+			logger.info("Current User is not Logined");
+			
+			saveDest(request);
+			response.sendRedirect("/user/login");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	// 로그인 성공 후 원래 가려고 했던 URI로 이동하도록 처리
+	private void saveDest(HttpServletRequest request) {
+		String uri = request.getRequestURI();
+		String query = request.getQueryString();
+		
+		if(query == null || query.equals("null")) {
+			query = "";
+		} else {
+			query = "?" + query;
+		}
+		
+		if(request.getMethod().equals("GET")) {
+			logger.info("desg : " + (uri + query));
+			request.getSession().setAttribute("dest", uri+query);
+		}
+	}
+
+}
